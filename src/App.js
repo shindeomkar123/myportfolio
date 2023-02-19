1e5de89ec629d7ce5430dc4a651a7681e76b2f45
Omkar Laxman Shinde <omkar.shinde@yash.com>
2023-02-19 11:47:48 +0530

    Add files

--- src/App.js
+++ src/App.js
@@ -9,19 +9,29 @@ const App = () => {
   return (
     <>
       {/* // Introduction page */}
-      <section className=" flex flex-col bg-gradient-to-r from-fuchsia-600 to-purple-600 w-full h-screen background-animate">
+      <section className=' flex flex-col w-full h-screen  bg-[url("./public/pxfuel.jpg")]'>
         <Header />
         <div className="flex flex-col h-4/5 justify-center ml-20">
-          <span className="text-2xl tracking-[20px] text-white font-bold">
+          <span className="text-5xl tracking-[20px] text-white font-bold">
             OMKAR SHINDE
           </span>
-          <TypeWriterEffect
-            startDelay={100}
-            cursorColor="black"
-            text="FRONTEND DEVELOPER"
-            typeSpeed={100}
-            eraseSpeed={100}
-          />{" "}
+          <div className="mt-5">
+            <TypeWriterEffect
+              textStyle={{
+                fontFamily: "Red Hat Display",
+                color: "yellow",
+                fontWeight: 500,
+                fontSize: "1.5em",
+              }}
+              startDelay={100}
+              cursorColor="yellow"
+              text="FRONTEND DEVELOPER"
+              typeSpeed={500}
+              eraseSpeed={100}
+              loop="true"
+              autostart="true"
+            />
+          </div>
         </div>
       </section>
 
